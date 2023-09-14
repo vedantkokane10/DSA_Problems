f2cdf07af8da0bc74d77798c2dab461a6b8b89ec
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2023-09-14 22:43:49 +0530

    Create 1. Largest.cpp

---
+++ Arrays/1. Largest.cpp
@@ -0,0 +1,10 @@
+int largest(vector<int> &arr, int n)
+    {
+        int max = arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
