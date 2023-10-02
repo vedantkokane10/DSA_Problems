0ee2d8fc36ad784d23930e8517a0feb473ca54da
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2023-10-02 23:41:38 +0530

    Create 5. Find the Smallest Divisor Given a Threshold.cpp

---
+++ Binary Search/BS on Answers (Medium)/5. Find the Smallest Divisor Given a Threshold.cpp
@@ -0,0 +1,39 @@
+/*
+Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, and sum the division's result. 
+Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
+Each result of the division is rounded to the nearest integer greater than or equal to that element. (For example: 7/3 = 3 and 10/2 = 5).
+The test cases are generated so that there will be an answer.
+
+Input: nums = [1,2,5,9], threshold = 6
+Output: 5
+Explanation: We can get a sum to 17 (1+2+5+9) if the divisor is 1. 
+If the divisor is 4 we can get a sum of 7 (1+1+2+3) and if the divisor is 5 the sum will be 5 (1+1+1+2). 
+
+Input: nums = [44,22,33,11,1], threshold = 5
+Output: 44
+*/
+
+int func(vector<int>& nums, int mid){
+    int sum = 0;
+    for(int i=0;i<nums.size();i++){
+        sum += (nums[i]/mid) + (nums[i]%mid != 0);
+    }
+    return sum;
+}
+int smallestDivisor(vector<int>& nums, int threshold) {
+    int low = 1;
+    int high = *max_element(nums.begin(),nums.end());
+    int ans=0;
+    while(low<=high){
+        int mid = (low+high)/2;
+        int x= func(nums,mid);
+        if(x <= threshold){
+            ans  = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    return ans;
+    }
