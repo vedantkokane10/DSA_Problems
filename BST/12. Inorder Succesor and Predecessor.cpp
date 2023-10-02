3fa0287694ae3e2172898bbaea1b97f21efc9f73
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2023-10-02 14:30:23 +0530

    Update 12. Inorder Succesor and Predecessor.cpp

--- BST/12. Inorder Succesor and Predecessor.cpp
+++ BST/12. Inorder Succesor and Predecessor.cpp
@@ -1,3 +1,25 @@
+/*
+There is BST given with the root node with the key part as an integer only. You need to find the in-order successor and predecessor of a given key. 
+If either predecessor or successor is not found, then set it to NULL.
+Note:- In an inorder traversal the number just smaller than the target is the predecessor and the number just greater than the target is the successor. 
+
+Input:
+        10
+      /   \
+     2    11
+   /  \ 
+  1    5
+      /  \
+     3    6
+      \
+       4
+key = 8
+Output: 
+6 10
+Explanation: 
+In the given BST the inorder predecessor of 8 is 6 and inorder successor of 8 is 10.
+*/
+
 void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
     
         Node* t1 = root;
